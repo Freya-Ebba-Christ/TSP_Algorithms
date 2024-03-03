778abed8a5f545a133dcc3e4811c137e2c66011c
Freya Ebba <57752514+Freya-Ebba-Christ@users.noreply.github.com>
2024-03-03 15:35:10 +0100

    Add files via upload

---
+++ ThreeOpt.java
@@ -0,0 +1,285 @@
+/*
+                   GNU LESSER GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+
+  This version of the GNU Lesser General Public License incorporates
+the terms and conditions of version 3 of the GNU General Public
+License, supplemented by the additional permissions listed below.
+
+  0. Additional Definitions.
+
+  As used herein, "this License" refers to version 3 of the GNU Lesser
+General Public License, and the "GNU GPL" refers to version 3 of the GNU
+General Public License.
+
+  "The Library" refers to a covered work governed by this License,
+other than an Application or a Combined Work as defined below.
+
+  An "Application" is any work that makes use of an interface provided
+by the Library, but which is not otherwise based on the Library.
+Defining a subclass of a class defined by the Library is deemed a mode
+of using an interface provided by the Library.
+
+  A "Combined Work" is a work produced by combining or linking an
+Application with the Library.  The particular version of the Library
+with which the Combined Work was made is also called the "Linked
+Version".
+
+  The "Minimal Corresponding Source" for a Combined Work means the
+Corresponding Source for the Combined Work, excluding any source code
+for portions of the Combined Work that, considered in isolation, are
+based on the Application, and not on the Linked Version.
+
+  The "Corresponding Application Code" for a Combined Work means the
+object code and/or source code for the Application, including any data
+and utility programs needed for reproducing the Combined Work from the
+Application, but excluding the System Libraries of the Combined Work.
+
+  1. Exception to Section 3 of the GNU GPL.
+
+  You may convey a covered work under sections 3 and 4 of this License
+without being bound by section 3 of the GNU GPL.
+
+  2. Conveying Modified Versions.
+
+  If you modify a copy of the Library, and, in your modifications, a
+facility refers to a function or data to be supplied by an Application
+that uses the facility (other than as an argument passed when the
+facility is invoked), then you may convey a copy of the modified
+version:
+
+   a) under this License, provided that you make a good faith effort to
+   ensure that, in the event an Application does not supply the
+   function or data, the facility still operates, and performs
+   whatever part of its purpose remains meaningful, or
+
+   b) under the GNU GPL, with none of the additional permissions of
+   this License applicable to that copy.
+
+  3. Object Code Incorporating Material from Library Header Files.
+
+  The object code form of an Application may incorporate material from
+a header file that is part of the Library.  You may convey such object
+code under terms of your choice, provided that, if the incorporated
+material is not limited to numerical parameters, data structure
+layouts and accessors, or small macros, inline functions and templates
+(ten or fewer lines in length), you do both of the following:
+
+   a) Give prominent notice with each copy of the object code that the
+   Library is used in it and that the Library and its use are
+   covered by this License.
+
+   b) Accompany the object code with a copy of the GNU GPL and this license
+   document.
+
+  4. Combined Works.
+
+  You may convey a Combined Work under terms of your choice that,
+taken together, effectively do not restrict modification of the
+portions of the Library contained in the Combined Work and reverse
+engineering for debugging such modifications, if you also do each of
+the following:
+
+   a) Give prominent notice with each copy of the Combined Work that
+   the Library is used in it and that the Library and its use are
+   covered by this License.
+
+   b) Accompany the Combined Work with a copy of the GNU GPL and this license
+   document.
+
+   c) For a Combined Work that displays copyright notices during
+   execution, include the copyright notice for the Library among
+   these notices, as well as a reference directing the user to the
+   copies of the GNU GPL and this license document.
+
+   d) Do one of the following:
+
+       0) Convey the Minimal Corresponding Source under the terms of this
+       License, and the Corresponding Application Code in a form
+       suitable for, and under terms that permit, the user to
+       recombine or relink the Application with a modified version of
+       the Linked Version to produce a modified Combined Work, in the
+       manner specified by section 6 of the GNU GPL for conveying
+       Corresponding Source.
+
+       1) Use a suitable shared library mechanism for linking with the
+       Library.  A suitable mechanism is one that (a) uses at run time
+       a copy of the Library already present on the user's computer
+       system, and (b) will operate properly with a modified version
+       of the Library that is interface-compatible with the Linked
+       Version.
+
+   e) Provide Installation Information, but only if you would otherwise
+   be required to provide such information under section 6 of the
+   GNU GPL, and only to the extent that such information is
+   necessary to install and execute a modified version of the
+   Combined Work produced by recombining or relinking the
+   Application with a modified version of the Linked Version. (If
+   you use option 4d0, the Installation Information must accompany
+   the Minimal Corresponding Source and Corresponding Application
+   Code. If you use option 4d1, you must provide the Installation
+   Information in the manner specified by section 6 of the GNU GPL
+   for conveying Corresponding Source.)
+
+  5. Combined Libraries.
+
+  You may place library facilities that are a work based on the
+Library side by side in a single library together with other library
+facilities that are not Applications and are not covered by this
+License, and convey such a combined library under terms of your
+choice, if you do both of the following:
+
+   a) Accompany the combined library with a copy of the same work based
+   on the Library, uncombined with any other library facilities,
+   conveyed under the terms of this License.
+
+   b) Give prominent notice with the combined library that part of it
+   is a work based on the Library, and explaining where to find the
+   accompanying uncombined form of the same work.
+
+  6. Revised Versions of the GNU Lesser General Public License.
+
+  The Free Software Foundation may publish revised and/or new versions
+of the GNU Lesser General Public License from time to time. Such new
+versions will be similar in spirit to the present version, but may
+differ in detail to address new problems or concerns.
+
+  Each version is given a distinguishing version number. If the
+Library as you received it specifies that a certain numbered version
+of the GNU Lesser General Public License "or any later version"
+applies to it, you have the option of following the terms and
+conditions either of that published version or of any later version
+published by the Free Software Foundation. If the Library as you
+received it does not specify a version number of the GNU Lesser
+General Public License, you may choose any version of the GNU Lesser
+General Public License ever published by the Free Software Foundation.
+
+  If the Library as you received it specifies that a proxy can decide
+whether future versions of the GNU Lesser General Public License shall
+apply, that proxy's public statement of acceptance of any version is
+permanent authorization for you to choose that version for the
+Library.
+
+ */
+/**
+ * Copyright (C) <2024>  <Freya Ebba Christ>
+ * @author Freya Ebba Christ
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class ThreeOpt {
+
+    public static int calculateTotalDistance(List<Integer> route, int[][] distanceMatrix) {
+        int totalDistance = 0;
+        for (int i = 0; i < route.size(); i++) {
+            totalDistance += distanceMatrix[route.get(i)][route.get((i + 1) % route.size())];
+        }
+        return totalDistance;
+    }
+
+    public static List<Integer> opt3(List<Integer> route, int[][] distanceMatrix) {
+        int bestDistance = calculateTotalDistance(route, distanceMatrix);
+        boolean improved = true;
+        while (improved) {
+            improved = false;
+            for (int i = 1; i < route.size() - 2; i++) {
+                for (int j = i + 1; j < route.size() - 1; j++) {
+                    for (int k = j + 1; k < route.size(); k++) {
+                        List<Integer> newRoute1 = new ArrayList<>(route.subList(0, i));
+                        List<Integer> newRoute2 = new ArrayList<>(route.subList(i, j));
+                        List<Integer> newRoute3 = new ArrayList<>(route.subList(j, k));
+                        List<Integer> newRoute4 = new ArrayList<>(route.subList(k, route.size()));
+
+                        // Case 1
+                        newRoute2 = reverse(newRoute2);
+                        newRoute3 = reverse(newRoute3);
+                        List<Integer> case1 = merge(newRoute1, newRoute2, newRoute3, newRoute4);
+
+                        // Case 2
+                        newRoute3 = route.subList(j, k);
+                        newRoute2 = route.subList(i, j);
+                        List<Integer> case2 = merge(newRoute1, newRoute2, newRoute3, newRoute4);
+
+                        // Case 3
+                        newRoute3 = reverse(newRoute3);
+                        List<Integer> case3 = merge(newRoute1, newRoute2, newRoute3, newRoute4);
+
+                        // Case 4
+                        newRoute2 = reverse(newRoute2);
+                        List<Integer> case4 = merge(newRoute1, newRoute2, newRoute3, newRoute4);
+
+                        List<List<Integer>> newRoutes = Arrays.asList(case1, case2, case3, case4);
+                        for (List<Integer> newRoute : newRoutes) {
+                            int newDistance = calculateTotalDistance(newRoute, distanceMatrix);
+                            if (newDistance < bestDistance) {
+                                route = newRoute;
+                                bestDistance = newDistance;
+                                improved = true;
+                                break; // Exit the for-loop and start over
+                            }
+                        }
+                        if (improved) break;
+                    }
+                    if (improved) break;
+                }
+                if (improved) break;
+            }
+        }
+        return route;
+    }
+
+    public static List<Integer> reverse(List<Integer> list) {
+        List<Integer> reversed = new ArrayList<>(list);
+        for (int i = 0; i < list.size() / 2; i++) {
+            int temp = reversed.get(i);
+            reversed.set(i, reversed.get(list.size() - i - 1));
+            reversed.set(list.size() - i - 1, temp);
+        }
+        return reversed;
+    }
+
+    public static List<Integer> merge(List<Integer>... lists) {
+        List<Integer> merged = new ArrayList<>();
+        for (List<Integer> list : lists) {
+            merged.addAll(list);
+        }
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int numCities = 5;
+        int[][] distanceMatrix = new int[numCities][numCities];
+        Random rand = new Random(42); // For reproducibility
+
+        // Generate random distance matrix
+        for (int i = 0; i < numCities; i++) {
+            for (int j = i + 1; j < numCities; j++) {
+                int distance = rand.nextInt(100) + 1;
+                distanceMatrix[i][j] = distance;
+                distanceMatrix[j][i] = distance; // Symmetric
+            }
+        }
+
+        // Initial route
+        List<Integer> initialRoute = new ArrayList<>();
+        for (int i = 0; i < numCities; i++) {
+            initialRoute.add(i);
+        }
+
+        // Solve using the 3-opt algorithm
+        List<Integer> optRoute = opt3(initialRoute, distanceMatrix);
+        int optDistance = calculateTotalDistance(optRoute, distanceMatrix);
+
+        System.out.println("Optimized route: " + optRoute);
+        System.out.println("Optimized distance: " + optDistance);
+    }
+}
+
