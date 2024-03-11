1a6346232eee7b093075ef9c4b451d67ef126d8d
Freya Ebba <57752514+Freya-Ebba-Christ@users.noreply.github.com>
2024-03-12 00:29:58 +0100

    Update TSP_GA_V17_LKH_bayes_optimization.py

--- TSP_GA_V17_LKH_bayes_optimization.py
+++ TSP_GA_V17_LKH_bayes_optimization.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 03.02.2024
+
+<This software implements spike sorting and detection a la Quiroga: http://www.scholarpedia.org/article/Spike_sorting>
+It is based on the MCS Tutorial: https://mcspydatatools.readthedocs.io/en/latest/McsPy-Tutorial_DataAnalysis.html
+
+Copyright (C) <2024>  <Freya Ebba Christ>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+@author: Freya Ebba Christ
+"""
+
 import numpy as np
 import random
 import heapq
